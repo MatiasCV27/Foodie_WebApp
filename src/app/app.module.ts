174c4a086b29dd5fec2b299e088b275bd7f08f68
Matias Criollo <matiascriollo57@gmail.com>
2024-03-16 23:09:57 -0500

    Moviendo las carpetas para su facil uso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { HomeComponent } from './pages/home/home.component';
-import { RestaurantesComponent } from './pages/usuario/restaurantes/restaurantes.component';
-import { MenuRestComponent } from './pages/usuario/menu-rest/menu-rest.component';
-import { UserPerfilComponent } from './pages/usuario/perfil/user-perfil/user-perfil.component';
-import { UserHistorialComponent } from './pages/usuario/perfil/user-historial/user-historial.component';
-import { UserAyudaComponent } from './pages/usuario/perfil/user-ayuda/user-ayuda.component';
-import { UserPreguntasComponent } from './pages/usuario/perfil/user-preguntas/user-preguntas.component';
-import { UserInfoComponent } from './pages/usuario/perfil/user-info/user-info.component';
+import { RestaurantesComponent } from './pages/restaurantes/restaurantes.component';
+import { MenuRestComponent } from './pages/menu-rest/menu-rest.component';
+import { UserHistorialComponent } from './pages/usuario/user-historial/user-historial.component';
+import { UserAyudaComponent } from './pages/usuario/user-ayuda/user-ayuda.component';
+import { UserPreguntasComponent } from './pages/usuario/user-preguntas/user-preguntas.component';
+import { UserInfoComponent } from './pages/usuario/user-info/user-info.component';
+import { UserDashboardComponent } from './pages/usuario/user-dashboard/user-dashboard.component';
 import { GestionRestComponent } from './pages/admin/gestion-rest/gestion-rest.component';
 import { GestionMenuComponent } from './pages/admin/gestion-menu/gestion-menu.component';
 import { GestionUserComponent } from './pages/admin/gestion-user/gestion-user.component';
@@ -37,11 +37,11 @@ import { GestionRestAsociadosComponent } from './pages/admin/gestion-rest-asocia
     HomeComponent,
     RestaurantesComponent,
     MenuRestComponent,
-    UserPerfilComponent,
     UserHistorialComponent,
     UserAyudaComponent,
     UserPreguntasComponent,
     UserInfoComponent,
+    UserDashboardComponent,
     GestionRestComponent,
     GestionMenuComponent,
     GestionUserComponent,
