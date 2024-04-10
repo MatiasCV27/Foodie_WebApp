d5a3534a1815a296ba662699ae1999b45fa6f2c8
Matias Criollo <matiascriollo57@gmail.com>
2024-04-09 19:22:41 -0500

    Conectando con Firebase, haciendo login/register con email y password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent { 
+
+  formLogin: FormGroup;
+
+  constructor(private userservices: UserService, private router: Router) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  onSubmit() {
+    this.userservices.login(this.formLogin.value)
+      .then(response => {
+        console.log(response);
+        this.router.navigate(['']);
+        Swal.fire({
+          icon: 'success',
+          title: 'Inicio de Sesión Exitoso',
+          text: '¡Bienvenido de vuelta!',
+          timer: 2000 ,
+          background: '#212529',
+          color: 'white'
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error en inicio de sesión',
+          text: 'Correo electrónico o contraseña incorrectos. Por favor, verifica tus credenciales.',
+          background: '#212529',
+          color: 'white'
+        });
+      });
+  }
 
 }
