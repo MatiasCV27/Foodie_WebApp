430f6a214ff014afbb169d78f4712f23dc578aed
Betzabe Yadhira Ramos Huapaya <SM72724365@idat.edu.pe>
2024-04-12 00:55:40 -0500

    Mantenimiento de Menus y Asociados

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  getMen() {
+    return this.firestore.collection("restPlatos").snapshotChanges();
+  }
+
+  createMen(menu: any){
+    return this.firestore.collection("restPlatos").add(menu);
+  }
+
+  updateMen(id: any, menu: any){
+    return this.firestore.collection("restPlatos").doc(id).update(menu);
+  }
+
+  deleteMen(id: any) {
+    return this.firestore.collection("restPlatos").doc(id).delete();
+  }
+
 }
