d246d9ae3889cff303f88c82ff45ce1ccafe51d5
InkShadow19 <SM74726725@idat.edu.pe>
2024-04-11 23:29:36 -0500

    Vista Restaurantes Funcionando

--- src/app/pages/menu-rest/menu-rest.component.ts
+++ src/app/pages/menu-rest/menu-rest.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-menu-rest',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-rest.component.css']
 })
 export class MenuRestComponent {
+
+  collection = { count: 6, data: [] as any[] };
+
+  restPlatos: any[] = [];
+
+  constructor(private restServices: MenuService) {}
+
+  ngOnInit(): void {
+    this.restServices.getRest().subscribe(
+      (respuesta: any[]) => {
+        this.restPlatos = respuesta.map((e: any) => ({
+          idDoc: e.payload.doc.id,
+          canitdad: e.payload.doc.data().canitdad,
+          idRest: e.payload.doc.data().idRest,
+          nombre: e.payload.doc.data().nombre,
+          precio: e.payload.doc.data().precio,
+          imagen: e.payload.doc.data().imagen
+        }));
+        this.collection.data = this.restPlatos;
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  filtrarPorRestaurante(idRest: string): void {
+    if (idRest === 'Todos') {
+      this.collection.data = this.restPlatos;
+    } else {
+      this.collection.data = this.restPlatos.filter(idRest => idRest.idRest === idRest);
+    }
+  }
 
 }
--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  getRest() {
+    return this.firestore.collection("restPlatos").snapshotChanges();
+  }
+
+  createRest(restaurante: any) {
+    return this.firestore.collection("restPlatos").add(restaurante);
+  }
+
+  updateRest(id: any, restaurante: any) {
+    return this.firestore.collection("restPlatos").doc(id).update(restaurante);
+  }
+
+  deleteRest(id: any) {
+    return this.firestore.collection("restPlatos").doc(id).delete();
+  }
 }
