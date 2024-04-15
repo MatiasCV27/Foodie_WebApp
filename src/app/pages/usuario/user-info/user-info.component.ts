3bb43e90c07ac5742f9a0e4c5fbc4d73d7882652
Matias Criollo <matiascriollo57@gmail.com>
2024-04-15 02:19:42 -0500

    Permisos

--- src/app/pages/usuario/user-info/user-info.component.ts
+++ src/app/pages/usuario/user-info/user-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-user-info',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent {
+
+  constructor(dataService: DataService) {
+    
+  }
 
 }
